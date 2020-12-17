0b00f1f8f4dbb33c41843a1201dc13f62e699fb8
saul205 <saulflobe@hotmail.com>
2020-12-17 22:58:00 +0100

    added max operation

--- src/main/java/soa/eip/Router.java
+++ src/main/java/soa/eip/Router.java
@@ -1,6 +1,8 @@
 package soa.eip;
 
 import org.apache.camel.builder.RouteBuilder;
+import org.apache.camel.Exchange;
+import org.apache.camel.Processor;
 import org.springframework.stereotype.Component;
 
 @Component
@@ -12,6 +14,26 @@ public class Router extends RouteBuilder {
   public void configure() {
     from(DIRECT_URI)
       .log("Body contains \"${body}\"")
+      .process(
+        new Processor(){
+          public void process(Exchange ex) throws Exception {
+            String body = ex.getIn().getBody(String.class);
+            String[] tokens = body.split(" ");
+
+            String newBody = "";
+            String max = "";
+            for(String t : tokens){
+              if(t.matches("max:[0-9]+")){
+                max = "?count=" + t.substring(4);
+              }else{
+                newBody += t;
+              }
+            }
+
+            ex.getOut().setBody(newBody + max);
+          }
+        }
+      )
       .log("Searching twitter for \"${body}\"!")
       .toD("twitter-search:${body}")
       .log("Body now contains the response from twitter:\n${body}");
